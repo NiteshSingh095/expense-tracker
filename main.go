20ac9963d32d1d1e4095998665c69c22455e7dd8
nitesh095 <anshukumargautam095@gmail.com>
2026-03-31 03:02:15 +0530

    CLI Command Parsing

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a command: add, list, delete, summary")
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "add":
+		addCommand()
+
+	case "list":
+		fmt.Println("List command triggered")
+
+	case "delete":
+		fmt.Println("Delete command triggered")
+
+	case "summary":
+		fmt.Println("Summary command triggered")
+
+	default:
+		fmt.Println("Unknown command. Available commands: add, list, delete, summary")
+	}
+}
+
+/// addCommand handles the 'add' command to add a new expense with amount and description.
+func addCommand() {
+	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+
+	amount := addCmd.Float64("amount", 0.0, "Expense Amount")
+	description := addCmd.String("description", "", "Expense Description")
+
+	addCmd.Parse(os.Args[2:])
+
+	if *description == "" || *amount <= 0 {
+		fmt.Println("Invalid Input. Description and Positive Amount are required.")
+		return
+	}
+
+	fmt.Println("Amount:", *amount)
+	fmt.Println("Description:", *description)
 }
