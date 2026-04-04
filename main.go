e5eb2d786952c683bae05b092710b2cf3b8eeae6
nitesh095 <anshukumargautam095@gmail.com>
2026-04-04 21:29:42 +0530

    UPDATE Command

--- main.go
+++ main.go
@@ -30,9 +30,65 @@ func main() {
 	case "summary":
 		summaryCommand()
 
+	case "update":
+		updateCommand()
+
 	default:
 		fmt.Println("Unknown command. Available commands: add, list, delete, summary")
 	}
+}
+
+/// updateCommand handles the 'update' command to modify an existing expense by its ID.
+func updateCommand() {
+	updateCmd := flag.NewFlagSet("update", flag.ExitOnError)
+
+	id := updateCmd.Int("id", 0, "Expense ID")
+	desc := updateCmd.String("description", "", "New Description")
+	amount := updateCmd.Float64("amount", 0, "New Amount")
+
+	updateCmd.Parse(os.Args[2:])
+
+	if *id <= 0 {
+		fmt.Println("Invalid Input. Positive ID is required.")
+		return
+	}
+
+	expenses, err := internal.LoadExpense()
+	if err != nil {
+		fmt.Printf("Error loading expenses: %s\n", err)
+		return
+	}
+
+	found := false
+
+	for index, expense := range expenses {
+		if expense.ID == *id {
+			found = true
+
+			if *desc != "" {
+				expenses[index].Description = *desc
+			}
+
+			if *amount > 0 {
+				expenses[index].Amount = *amount
+			}
+
+			break
+		}
+	}
+
+	if !found {
+		fmt.Printf("Expense with ID %d not found.\n", *id)
+		return
+	}
+
+	err = internal.SaveExpense(expenses)
+	if err != nil {
+		fmt.Printf("Error saving expenses: %s\n", err)
+		return
+	}
+
+	fmt.Printf("Expense with ID %d updated successfully!\n", *id)
 }
 
 /// summaryCommand handles the 'summary' command to calculate and display total expenses, optionally filtered by month.
